823e091ba0575a469936a5c1e557b70ed80ed5ea
Seger01 <sjw.sars@student.avans.nl>
2024-06-16 12:47:40 +0200

    Dynamic grid size functionality is implemented

--- src/main.py
+++ src/main.py
@@ -1,24 +1,27 @@
 import pygame
 import sys
-
 # from ships import BaseShip
 # from GameBoard import GameBoard
 from Menu import Menu
 from Database import Database, GameBoard
 from functools import partial
+
 # Initialize Pygame
 pygame.init()
+
 SCREEN_HEIGHT = 1000
 SCREEN_WIDTH = 1500
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # screen = ScreenWrapper(SCREEN_WIDTH, SCREEN_HEIGHT)
-game_board = GameBoard(screen, 10, 10)
 
 previous_games_list = []
+grid_size = 10
 
 
 def game_start():
     print("Start Game!")
+    game_board = GameBoard(screen, grid_size, grid_size)
     game_board.player.set_nickname(menu.text_input)
     game_board.run()
 
@@ -43,12 +46,49 @@ def see_previous_games():
 
 
 def exit_game():
-    # TODO actually quit the application
+    # current_function = menu_items_list[2][1]
+    #
+    # new_tuple = ("Smexit", current_function)
+    #
+    # menu_items_list[2] = new_tuple   # TODO actually quit the application
     print("Exitting game")
 
 
+def current_grid_size():
+    global grid_size
+    current_function = menu_items_list[3][1]
+
+    new_tuple = (f"Current gridsize: {grid_size}", current_function)
+
+    menu_items_list[3] = new_tuple   # TODO actually quit the application
+
+
+def increase_grid_size():
+    global grid_size
+    grid_size += 1
+
+    if grid_size >= 16:
+        grid_size = 16
+
+    current_grid_size()
+    return
+
+
+def decrease_grid_size():
+    global grid_size
+    grid_size -= 1
+
+    if grid_size <= 8:
+        grid_size = 8
+    current_grid_size()
+    return
+
+
+menu_items_list = ([("Start game", game_start), ("See previous games", see_previous_games),
+                   ("Exit", exit_game), ("Current gridsize: 10", current_grid_size),
+                    ("Increase gridsize", increase_grid_size), ("Decrease gridsize", decrease_grid_size)])
 # Create the menu with options and associated functions
-menu = Menu(screen, [("Start game", game_start), ("See previous games", see_previous_games), ("Exit", exit_game)])
+menu = Menu(screen, menu_items_list)
 
 # Run the menu
 menu.run()
