c382c1b5b23d3910af4de7aa50f9ab4f68e8178a
UnavailableDev <69792062+UnavailableDev@users.noreply.github.com>
2024-06-16 11:39:38 +0200

    Colors + random ship init

---
+++ src/Defines.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class colors(Enum):
+    HIT = (255, 0, 0)
+    SHOT = (0, 255, 255)
+    SHIP = (255, 255, 255)
+    VISIBLE = (128, 128, 192)
+    GRID = (48, 69, 77)
+    UNK_SHIP = (128, 69, 128)
--- src/Player.py
+++ src/Player.py
@@ -1,15 +1,10 @@
 from ships import *
 from datatypes import Position as Pos
+from Defines import colors
+import random
 
 
 class Player:
-    __HIT_COLOR = (255, 0, 0)
-    __SHOT_COLOR = (0, 255, 255)
-    __SHIP_COLOR = (255, 255, 255)
-    __VISIBLE_COLOR = (128, 128, 192)
-    __GRID_COLOR = (48, 69, 77)
-    __UNK_SHIP_COLOR = (128, 69, 128)
-
     rows = 0
     cols = 0
 
@@ -25,23 +20,25 @@ class Player:
             self.shots.append(inner_array)
 
         self.ships.append(ScoutShip())
-        # self.ships.append(ScoutShip())
-        # self.ships.append(HunterShip())
-        # self.ships.append(HunterShip())
-        # self.ships.append(CruiserShip())
-        # self.ships.append(CruiserShip())
-        # self.ships.append(BattleShip())
-        # self.ships.append(CommandShip())
+        self.ships.append(ScoutShip())
+        self.ships.append(HunterShip())
+        self.ships.append(HunterShip())
+        self.ships.append(CruiserShip())
+        self.ships.append(CruiserShip())
+        self.ships.append(BattleShip())
+        self.ships.append(CommandShip())
 
         # TODO make this dynamic / from the outside
-        # self.ships[7].set_position(Pos(0 ,0))
-        # self.ships[6].set_position(Pos(0 ,1))
-        # self.ships[5].set_position(Pos(0 ,2))
-        # self.ships[4].set_position(Pos(0 ,3))
-        # self.ships[3].set_position(Pos(0 ,4))
-        # self.ships[2].set_position(Pos(0 ,5))
-        # self.ships[1].set_position(Pos(0 ,6))
-        self.ships[0].set_position(Pos(0 ,7))
+        self.ships[7].set_position(Pos(0 + self.cols, 0))
+        self.ships[6].set_position(Pos(0 + self.cols, 1))
+        self.ships[5].set_position(Pos(0 + self.cols, 2))
+        self.ships[4].set_position(Pos(0 + self.cols, 3))
+        self.ships[3].set_position(Pos(0 + self.cols, 4))
+        self.ships[2].set_position(Pos(0 + self.cols, 5))
+        self.ships[1].set_position(Pos(0 + self.cols, 6))
+        self.ships[0].set_position(Pos(0 + self.cols, 7))
+
+        self.rand_ship_layout()
 
 
     def shoot_ships(self, pos:Pos):
@@ -67,6 +64,42 @@ class Player:
         return False
 
 
+    def rand_ship_layout(self):
+        for i in range(len(self.ships)):
+            while not self.new_ship_pos(i):
+
+                self.ships[i].set_position(Pos(self.rows, self.cols))
+
+            print(i, self.ships[i].get_position())
+    
+    def new_ship_pos(self, idx: int):
+        size = self.ships[idx].get_size()
+        hor: bool = bool(random.getrandbits(1))
+
+        # rand_x = random.randrange(self.rows)
+        # rand_y = random.randrange(self.rows)
+
+        rand_x = random.randrange(self.rows - size * hor)
+        rand_y = random.randrange(self.rows - size * (not hor))
+
+        # Edge violations ---->
+        # if (rand_x + size > self.rows or rand_y + size > self.cols):
+        #     return False
+        if hor and (self.rows <= rand_x + size):
+            print("NANI X", rand_x)
+            return False
+        if not hor and (self.cols <= rand_y + size):
+            print("NANI Y", rand_y)
+            return False
+
+        print(size, Pos(rand_x, rand_y, hor))
+        self.ships[idx].set_position(Pos(rand_x, rand_y, hor))
+        if self.ships_colliding():
+            self.ships[idx].set_position(Pos(self.rows, self.cols))
+            return False
+
+        return True # Success
+
     def ships_colliding(self) -> bool:
         temp_array: bool = []
         for i in range(self.rows):
@@ -76,8 +109,11 @@ class Player:
             temp_array.append(inner_array)
 
         for i in range(len(self.ships)):
+            ship_pos = self.ships[i].get_position()
+            if (ship_pos.x >= self.rows) or (ship_pos.y >= self.cols):
+                return False # Ignore ships with starting point outside the grid
+
             for j in range(self.ships[i].get_size()):
-                ship_pos = self.ships[i].get_position()
                 if temp_array[ship_pos.y + (j * (not ship_pos.horizontal))][ship_pos.x +
                                                                             (j * (ship_pos.horizontal))] is True:
                     # print("SHIP COLLISION")
@@ -101,7 +137,7 @@ class Player:
             #
             #         elif self.ships[i].get_position().horizontal is False:
             #
-        return True
+        return False
 
     # TODO add True return if movement got accepted
     def move_ship(self, pos: Pos, vertical: int, horizontal: int) -> bool:
@@ -128,7 +164,7 @@ class Player:
 
         potential_moved_ship.set_position(ship_pos)
 
-        if self.ships_colliding() is True:
+        if not self.ships_colliding():
             potential_moved_ship.set_position(previous_ship_pos)
 
         return True
@@ -156,21 +192,37 @@ class Player:
         return self.shots[pos.x][pos.y]
 
     def get_grid_color(self, pos: Pos, enemy=False):
-        color = self.__GRID_COLOR
+        color = colors.GRID.value
         if enemy:
-            if self.get_grid_shot(pos):
-                color = self.__VISIBLE_COLOR
+            pass
+            # if self.get_grid_shot(pos):
+            #     color = colors.VISIBLE.value
             
-                # see if there is a ship
-                if self.get_grid_ship(pos):
-                    color = self.__UNK_SHIP_COLOR
+            #     # see if there is a ship
+            #     if self.get_grid_ship(pos):
+            #         color = colors.UNK_SHIP.value
 
-                    # see if the ship is hit in that position
-                    if None:
-                        color = self.__HIT_COLOR                
+            #         # see if the ship is hit in that position
+            #         if None:
+            #             color = colors.HIT.value          
         else:
             # see if there is a ship
             if self.get_grid_ship(pos):
-                color = self.__SHIP_COLOR
+                color = colors.SHIP.value
+
+        if self.get_grid_shot(pos):
+            if enemy:
+                color = colors.VISIBLE.value
+            else:
+                color = colors.SHOT.value
+            
+            if self.get_grid_ship(pos):
+                if enemy:
+                    color = colors.UNK_SHIP.value
+                    # see if the ship is hit in that position
+                    if None:
+                        color = colors.HIT.value 
+                else:
+                    color = colors.HIT.value
 
         return color
